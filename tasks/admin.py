f0338e28dd68ac840a456e69fde51b4fb6900dc4
catediana <catherinenanyala@gmail.com>
2025-08-30 08:36:36 +0300

    modified some files

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Category, Task, UserProfile,Subtask, Comment
 
-# Register your models here.
 admin.site.register(Task)
 admin.site.register(Subtask)
 admin.site.register(Category)
