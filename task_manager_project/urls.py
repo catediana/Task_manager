e2617eb4ffee7f7b2f6fd5caa8894993af8e253c
catediana <catherinenanyala@gmail.com>
2025-10-01 13:37:43 +0300

    changed the color combination amd modified the home.htm template and added the background image that applies to all the pages

--- task_manager_project/urls.py
+++ task_manager_project/urls.py
@@ -28,3 +28,4 @@ urlpatterns = [
 # Serve media files only during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
